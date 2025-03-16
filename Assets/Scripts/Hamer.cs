10c35781237118b56011f2630c6470ae5c55870e
Im <imjuday@gmail.com>
2025-03-16 13:30:13 +0700

    Add Haptic Feedback
    Player who were attacked will have their controller vibrate.

--- Assets/Scripts/Hamer.cs
+++ Assets/Scripts/Hamer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Hamer : MonoBehaviour
 {
@@ -25,6 +26,35 @@ public class Hamer : MonoBehaviour
                 targetRb.AddForce(-collision.transform.forward * force, ForceMode.Impulse);
             }
             acc += 5;
+
+           
+            PlayerInput playerInput = collision.gameObject.GetComponent<PlayerInput>();
+            if (playerInput != null)
+            {
+                Gamepad gamepad = playerInput.devices[0] as Gamepad;
+                if (gamepad != null)
+                {
+                    VibrateGamepad(gamepad, 0.5f, 0.7f, 1.0f);
+                }
+            }
+        }
+    }
+
+    void VibrateGamepad(Gamepad gamepad, float duration, float lowIntensity, float highIntensity)
+    {
+        if (gamepad != null)
+        {
+            gamepad.SetMotorSpeeds(lowIntensity, highIntensity);
+            StartCoroutine(StopVibration(gamepad, duration));
+        }
+    }
+
+    System.Collections.IEnumerator StopVibration(Gamepad gamepad, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (gamepad != null)
+        {
+            gamepad.SetMotorSpeeds(0, 0);
         }
     }
 }
--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Sword : MonoBehaviour
 {
     public float force, mass, acc;
-    public GameObject owner; 
+    public GameObject owner;
 
     void Start()
     {
@@ -25,6 +26,35 @@ public class Sword : MonoBehaviour
                 targetRb.AddForce(-collision.transform.forward * force, ForceMode.Impulse);
             }
             acc += 2;
+
+            
+            PlayerInput playerInput = collision.gameObject.GetComponent<PlayerInput>();
+            if (playerInput != null)
+            {
+                Gamepad gamepad = playerInput.devices[0] as Gamepad;
+                if (gamepad != null)
+                {
+                    VibrateGamepad(gamepad, 0.5f, 0.5f, 1.0f);
+                }
+            }
+        }
+    }
+
+    void VibrateGamepad(Gamepad gamepad, float duration, float lowIntensity, float highIntensity)
+    {
+        if (gamepad != null)
+        {
+            gamepad.SetMotorSpeeds(lowIntensity, highIntensity);
+            StartCoroutine(StopVibration(gamepad, duration));
+        }
+    }
+
+    System.Collections.IEnumerator StopVibration(Gamepad gamepad, float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (gamepad != null)
+        {
+            gamepad.SetMotorSpeeds(0, 0);
         }
     }
 }
